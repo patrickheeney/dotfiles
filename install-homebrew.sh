4c866a0e5ea236b51269c923934b9d254f340a31
patrickheeney <patrickheeney@gmail.com>
2014-11-07 16:22:41 -0700

    Revise homebrew install. Add osx options. More functions.

--- install-homebrew.sh
+++ install-homebrew.sh
@@ -6,6 +6,9 @@
 # Usage: ./homebrew/install.sh
 # 
 
+# Ask for the administrator password upfront.
+sudo -v
+
 # Clean things
 brew cleanup --force
 
@@ -22,27 +25,34 @@ brew doctor
 brew tap homebrew/dupes
 brew tap homebrew/versions
 
-# Install GNU core utilities (those that come with OS X are outdated)
+# Install GNU core utilities (those that come with OS X are outdated).
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-# install coreutils
+brew install coreutils
+sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
+
 # Install some other useful utilities like `sponge`
-# install moreutils
+brew install moreutils
+
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed
-# install findutils
+brew install findutils
+
 # Install GNU `sed`, overwriting the built-in `sed`
-# install gnu-sed --default-names
+brew install gnu-sed --default-names
+
 # Install Bash 4
 # Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before running `chsh`.
-# install bash
-# install bash-completion
+#brew install bash
+#brew install bash-completion
 
 # Install wget with IRI support
 brew install wget --enable-iri
 
 # Install more recent versions of some OS X tools
-# install vim --override-system-vi
+brew install vim --override-system-vi
+brew install grep
+brew install screen
 
-# Install php 5.5
+# Install PHP 5.5
 brew tap josegonzalez/homebrew-php
 brew install php55
 brew install mcrypt
@@ -50,32 +60,32 @@ brew install php55-mcrypt
 brew install composer
 
 # Install other useful binaries
-# install ack
-# install dos2unix
-# install exiv2
+brew install ack
 brew install git
-# install imagemagick
-# install lynx
-brew install node
-# install pigz
-# install rename
-# install rhino
+#brew install imagemagick --with-webp
+# brew install p7zip
+brew install pigz
+# brew install pv
+brew install rename
+#brew install rhino
 brew install tree
-# install webkit2png
-# install zopfli
-# install lua52
+#brew install webkit2png
+brew install unrar
+#brew install zopfli
 
-# Remove outdated versions from the cellar
-brew cleanup
+# Install Node.js. Note: this installs `npm` too, using the recommended
+# installation method.
+brew install node
 
 # 
 # Install native apps
 # 
 
+:'
 brew tap caskroom/cask
-brew install brew-cask
-# brew install caskroom/cask/brew-cask
 brew tap caskroom/versions
+
+brew install brew-cask
 
 brew cask install adobe-creative-cloud
 brew cask install adobe-reader
@@ -114,3 +124,7 @@ brew cask install transmit
 brew cask install vagrant
 brew cask install virtualbox
 brew cask install vlc
+'
+
+# Remove outdated versions from the cellar
+brew cleanup
