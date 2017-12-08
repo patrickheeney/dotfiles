9c23c64895d51ec602c150f4a77cc64469d314e1
Patrick Heeney <patrickheeney@gmail.com>
2017-12-08 12:23:55 -0700

    Git clone from https

--- bootstrap.sh
+++ bootstrap.sh
@@ -41,6 +41,7 @@ log "Starting bootstrap..."
 #
 if type xcode-select >&- && xpath=$( xcode-select --print-path ) && test -d "${xpath}" && test -x "${xpath}"; then
   log "Running xcode license..."
+  log "If it fails, try checking for software updates, or, opening xcode to install required components and manually accept license"
   sudo xcodebuild -license accept
 else
   log "Installing xcode tools..."
@@ -55,6 +56,8 @@ if ! has_program "brew"; then
   log "Installing homebrew..."
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   log "Finished installing homebrew"
+  log "Turning off homebrew analytics..."
+  brew analytics off
 fi
 
 #
@@ -69,7 +72,8 @@ fi
 #
 # Clone the repo
 #
-git clone git@github.com:patrickheeney/dotfiles.git $HOME/dotfiles
+#git clone git@github.com:patrickheeney/dotfiles.git $HOME/dotfiles
+git clone https://github.com/patrickheeney/dotfiles.git $HOME/dotfiles
 cd $HOME/dotfiles
 
 #
