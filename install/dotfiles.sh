9955bc147cd0ba7b35a4c45ab1a1d64874838e1c
Patrick Heeney <patrickheeney@gmail.com>
2017-12-08 14:17:36 -0700

    Debug

--- install/dotfiles.sh
+++ install/dotfiles.sh
@@ -20,20 +20,23 @@ echo "Symlinking dotfiles from $DIR"
 
 # Symlink bash files
 find "$DIR/../bash" -type f -name '*' -print0 | while IFS= read -r -d '' location; do
-  newfile=$( basename location )
-  link "$DIR/$newfile" "$HOME/$newfile"
+  newfile=$( basename "$location" )
+  echo "$DIR/$newfile" "$HOME/$newfile"
+  #link "$DIR/$newfile" "$HOME/$newfile"
 done
 
 # Symlink git files
 find "$DIR/../git" -type f -name '*' -print0 | while IFS= read -r -d '' location; do
-  newfile=$( basename location )
-  link "$DIR/$newfile" "$HOME/$newfile"
+  newfile=$( basename "$location" )
+  echo "$DIR/$newfile" "$HOME/$newfile"
+  #link "$DIR/$newfile" "$HOME/$newfile"
 done
 
 # Symlink app files
 find "$DIR/../apps" -type f -name '*' -print0 | while IFS= read -r -d '' location; do
-  newfile=$( basename location )
-  link "$DIR/$newfile" "$HOME/$newfile"
+  newfile=$( basename "$location" )
+  echo "$DIR/$newfile" "$HOME/$newfile"
+  #link "$DIR/$newfile" "$HOME/$newfile"
 done
 
 # Reset settings
